ea285f576d18349bdbf1ab0c6bff956aa98611bf
mi-araf <arafmushfiq@gmail.com>
2022-11-18 12:14:19 +0600

    navigation route done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
     return (
         <div className='header'>
             <img src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Products</a>
+                <a href="/feedback">Feedback</a>
+            </nav>
         </div>
     );
 };
